6ed2acb75777502bbb2db63640e0866629c6496e
tria <e32241416@student.polije.ac.id>
2025-05-01 20:09:06 +0700

    yaya

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,18 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+//Dashboard
 Route::get('dashboard', [DashboardController::class, 'index'])->name('balik');
 
+//Login
 Route::get('login', [AuthController::class, 
 'login'])->name ('login');
 
+//User
 Route::get('user', [UserController::class,
 'index'])->name ('user');
+
+//Tugas
+Route::get('tugas', [TugasController::class,
+'tugas'])->name ('tugas');
+
