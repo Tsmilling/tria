206a61806b60a65fc9dd4bb6591c96493ea31629
tria <e32241416@student.polije.ac.id>
2025-05-01 19:14:40 +0700

    first

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,11 +1,71 @@
 @extends('layouts/app')
 
-@extends('layouts/app')
-
 @section('content')
     <h1 class="h3 mb-4 text-gray-800">
         <i class="fas fa-user-alt mr-2"></i>
         {{$title}}
     </h1>
+
+    <div class="card">
+        <div class="cadr-header d-flex flex-warp justify-content-center justify-content-xl-between">
+        <div>
+            <a href="#" class="btn btn-primary" style="margin-left: 10px;"">
+            <i class="fas fa-plus mr2"></i>
+                Tambah Data</a>
+        </div>   
+
+        <div>
+            <a href="#" class="btn btn-success" style="margin-left: 10px;"">
+            <i class="fas fa-file-excel mr2"></i>
+                Excel</a>
+            <a href="#" class="btn btn-danger" style="margin-left: 10px;"">
+            <i class="fas fa-file-excel mr2"></i>
+                PDF</a>
+        </div>  
+
+        
+        
+        
+        </div>
+        <div class="card-body">
+        <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead class="bg-primary text-white">
+                                        <tr class="text-center">
+                                            <th>Nama</th>
+                                            <th>Email</th>
+                                            <th>Jabatan</th>
+                                            <th>Status</th>
+                                            <th>
+                                                <i class="fas fa-cog"
+                                            </th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td class="text-center">
+                                            <span class="badge badge-dark badge-pill">Admin</span>
+                                            </td>
+                                            
+                                            <td class="text-center">
+                                            <span class="badge badge-danger badge-pill">Belum di Tugaskan</span>
+                                            </td>
+                                            <td class="text-center">
+                                                <a href="#" class="btn btn-warning btn-sm">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <a href="#" class="btn btn-danger btn-sm">
+                                                    <i class="fas fa-trash"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+        </div>
+    </div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('sbadmin2/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -210,6 +211,12 @@
     <script src="{{ asset ('sbadmin2/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset ('sbadmin2/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <script src="{{ asset ('sbadmin2/js/sb-admin-2.min.js') }}"></script>
+
+    <script src="{{ asset('sbadmin2/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('sbadmin2/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('sbadmin2/js/demo/datatables-demo.js') }}"></script>
 
 </body>
 
