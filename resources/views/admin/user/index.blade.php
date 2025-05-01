9304483ce5d09d801165b865fae9866663e853e1
tria <e32241416@student.polije.ac.id>
2025-05-01 09:49:30 +0700

    tria

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/app')
+
+@extends('layouts/app')
+
+@section('content')
+    <h1 class="h3 mb-4 text-gray-800">
+        <i class="fas fa-user-alt mr-2"></i>
+        {{$title}}
+    </h1>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('balik');
+
+Route::get('login', [AuthController::class, 
+'login'])->name ('login');
+
+Route::get('user', [UserController::class, 
+'index'])->name ('user');
